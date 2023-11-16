99d1da973e71158544200b6b2da23cc31b957d5d
Joserafpg <payerogarciaj@gmail.com>
2023-11-16 11:51:04 -0400

    Version 2.9

---
+++ Proyecto Gregory/Clases/Habitaciones/DatosgetHabitaciones.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Gregory.Clases.Habitaciones
+{
+    public class DatosgetHabitaciones
+    {
+        public Int64 Codigo { get; set; }
+        public string Numero_habitacion { get; set; }
+        public string Tipo_habitacion { get; set; }
+        public decimal Tarifa_noche { get; set; }
+        public Int64 Capacida_maxima { get; set; }
+        public Int64 Camas { get; set; }
+        public bool Servicio_habitacion { get; set; }
+        public string Estado { get; set; }
+
+        public DatosgetHabitaciones() { }
+
+        public DatosgetHabitaciones(Int64 pCodigo, string pNumero, string pTipo, decimal pTarifa, Int64 pCapacidad, Int64 pCamas, bool pServicio, string pEstado)
+        {
+            this.Codigo = pCodigo;
+            this.Numero_habitacion = pNumero;
+            this.Tipo_habitacion = pTipo;
+            this.Tarifa_noche = pTarifa;
+            this.Capacida_maxima = pCapacidad;
+            this.Camas = pCamas;
+            this.Servicio_habitacion = pServicio;
+            this.Estado = pEstado;
+        }
+    }
+}
--- Proyecto Gregory/Habitaciones.cs
+++ Proyecto Gregory/Habitaciones.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Proyecto_Gregory.Clases.Habitaciones;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,9 +19,75 @@ namespace Proyecto_Gregory
             InitializeComponent();
         }
 
+        public static SqlConnection Conn = new SqlConnection("Server = DESKTOP-NDDA7LS; database=Hotel; Integrated Security=True");
+
+        void CargarComboBox()
+        {
+            Conn.Open();
+            string consulta = "SELECT DISTINCT Tipo_habitacion FROM Habitaciones";
+            SqlCommand comando = new SqlCommand(consulta, Conn);
+            SqlDataReader lector = comando.ExecuteReader();
+
+            while (lector.Read())
+            {
+                cClase.Items.Add(lector.GetString(0));
+            }
+
+            Conn.Close();
+
+
+
+            Conn.Open();
+            string consulta2 = "SELECT DISTINCT Capacida_maxima FROM Habitaciones";
+            SqlCommand comando2 = new SqlCommand(consulta2, Conn);
+            SqlDataReader lector2 = comando2.ExecuteReader();
+
+            while (lector2.Read())
+            {
+                cCapacidad.Items.Add(lector2.GetSqlInt32(0));
+            }
+
+            Conn.Close();
+
+
+
+            Conn.Open();
+            string consulta3 = "SELECT DISTINCT Estado FROM Habitaciones";
+            SqlCommand comando3 = new SqlCommand(consulta3, Conn);
+            SqlDataReader lector3 = comando3.ExecuteReader();
+
+            while (lector3.Read())
+            {
+                cDisponibilidad.Items.Add(lector3.GetString(0));
+            }
+
+            Conn.Close();
+        }
+
+        void Buscar()
+        {
+            dataGridView1.DataSource = DatosbaseHabitaciones.BuscarAlumnos(txtBuscar.Text, cClase.Text, cCapacidad.Text, cDisponibilidad.Text);
+        }
+
         private void Habitaciones_Load(object sender, EventArgs e)
         {
+            Buscar();
+            CargarComboBox();
+        }
 
+        private void bunifuButton24_Click(object sender, EventArgs e)
+        {
+            Buscar();
+        }
+
+        private void txtBuscar_TextChanged(object sender, EventArgs e)
+        {
+            Buscar();
+        }
+
+        private void cClase_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Buscar();
         }
     }
 }
